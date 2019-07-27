1fc6df627c75e27f54e759afff4d2ca63aaf94c3
raul <raul0301@gmail.com>
2019-07-27 14:01:58 +0100

    Add home unifi

--- unifi/setup_mcvlan.sh
+++ unifi/setup_mcvlan.sh
@@ -5,19 +5,18 @@ then
 docker network create -d macvlan \
   --subnet=192.168.1.0/24 \
   --gateway=192.168.1.1 \
-  --ip-range=192.168.1.192/27 \
-  -o parent=enp4s0 \
-  --aux-address 'host=192.168.1.223' \
+  -o parent=eth0 \
  unifi-vlan 
+  #--aux-address 'host=192.168.1.223' \
+  #--ip-range=192.168.1.202 \
 fi
 
 
 ## add  bridge to my 192.168.1.223
-sudo ip link add unifi-bridge link enp4s0 type macvlan  mode bridge
+#sudo ip link add unifi-bridge link enp4s0 type macvlan  mode bridge
 ## Add address
-sudo ip addr add 192.168.1.223/32 dev unifi-bridge
+#sudo ip addr add 192.168.1.223/32 dev unifi-bridge
 ## bring it run
-sudo ip link set unifi-bridge up
+#sudo ip link set unifi-bridge up
 ## routes
-sudo ip route add  192.168.1.192/27 dev unifi-bridge
-
+#sudo ip route add  192.168.1.192/27 dev unifi-bridge
